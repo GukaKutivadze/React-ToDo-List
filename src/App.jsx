7ced8c96a9511085dd08c4d7d8bc512a89f74aaa
GukaKutivadze <guk1akutivadze@gmail.com>
2025-10-16 23:35:21 +0400

    Updated App.jsx and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState("");
+  const [items, setItems] = useState([
+    { text: "a", checked: false },
+    { text: "b", checked: false },
+    { text: "c", checked: false },
+  ]);
+
+  const handleSaveItem = () => {
+    if (value === "") {
+      alert("Value must not be empty");
+    } else {
+      setItems([...items, { text: value, checked: false }]);
+      setValue("");
+    }
+  };
+
+  const deleteItem = (indexToDelete) => {
+    setItems(items.filter((_, index) => index !== indexToDelete));
+  };
+
+  const handleInputChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const toggleCheck = (indexToToggle) => {
+    const updatedItems = items.map((item, index) =>
+      index === indexToToggle
+        ? { ...item, checked: !item.checked }
+        : item
+    );
+    setItems(updatedItems);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="addBar">
+        <h1>🌟To-Do List🌟</h1>
+        <input
+          type="text"
+          placeholder="Enter a new task..."
+          value={value}
+          onChange={handleInputChange}
+          className="input"
+        />
+        <button onClick={handleSaveItem}>Add</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <ul>
+        {items.map((item, index) => (
+          <li
+            key={index}
+            className={`todo-item ${item.checked ? "checked" : ""}`}
+          >
+            <input
+              type="checkbox"
+              checked={item.checked}
+              onChange={() => toggleCheck(index)}
+            />
+            <span>{item.text}</span>
+            <button
+              className="delete-btn"
+              onClick={() => deleteItem(index)}
+            >
+              ✖
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
